33f526d8a210de4ecb742e63ea7e3e3699b94c57
Ubuntu <ubuntu@ip-172-31-17-127.us-west-2.compute.internal>
2013-08-07 04:36:23 +0000

    update grader.js

--- grader.js
+++ grader.js
@@ -63,8 +63,8 @@ var clone = function(fn) {
 
 if(require.main == module) {
     program
-        .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .parse(process.argv);
     var checkJson = checkHtmlFile(program.file, program.checks);
     var outJson = JSON.stringify(checkJson, null, 4);
